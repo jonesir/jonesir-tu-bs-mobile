73bf7b99bdb9122da3fbafd150834b49b578149c
jonesir <jasonjone23@gmail.com>
2014-06-22 22:41:59 +0200

    navi links function for all faculties

--- fakultaeten.py
+++ fakultaeten.py
@@ -1,6 +1,6 @@
 import sys
 import json
-sys.path.insert(0,'libs')
+sys.path.insert(0, 'libs')
 from bs4 import BeautifulSoup 
 import webapp2 
 import urllib2
@@ -22,7 +22,7 @@ class Fakultaeten(webapp2.RequestHandler):
         self.response.headers['Content-Type'] = 'application/json'
         
         # get fakultaet number
-        fakultaet = int(self.request.url[len(self.request.host_url)+len('/fakultaeten/'):])
+        fakultaet = int(self.request.url[len(self.request.host_url) + len('/fakultaeten/'):])
         
         # do corresponding stuff according to fakultaet
         if fakultaet == 1:
@@ -38,45 +38,78 @@ class Fakultaeten(webapp2.RequestHandler):
         elif fakultaet == 6:
             self.fakultaet6()
             
-        # create and generate json string
-        result_json = {'fakultaet':fakultaet}
-        jsonString = json.dumps(result_json)
-        
     # Carl-Friedrich-Gauss-Fakultaet    
     def fakultaet1(self):
         html = urllib2.urlopen(FAKULTAET_1).read()
         soup = BeautifulSoup(html)
-        self.response.write(soup.prettify())
+        # navi links for fk1
+        naviLinks = self.getNaviLinks(soup)
+        # contents for fk1
+        contents = self.getInhalt(soup)
+        # links in content for fk1
+        contentLinks = self.getInhaltLinks(soup)
+        # json string of fk1 consists of 'navi', 'contents' and 'link in content'
+        self.response.write(json.dumps({'navi':naviLinks,'content':{'contents':contents, 'content_links':contentLinks}}))
         
     # Lebenswissenschaften
     def fakultaet2(self):
         html = urllib2.urlopen(FAKULTAET_2).read()
         soup = BeautifulSoup(html)
-        self.response.write(soup.prettify())
+        # navi links for fk2
+        naviLinks = self.getNaviLinks(soup)
+        # 
+        self.response.write(json.dumps({'navi':naviLinks}))
     
     # Architektur, Bauingenieurwesen und Umweltwissenschaften
     def fakultaet3(self):
         html = urllib2.urlopen(FAKULTAET_3).read()
         soup = BeautifulSoup(html)
-        self.response.write(soup.prettify())
+        naviLinks = self.getNaviLinks(soup)
+        self.response.write(json.dumps({'navi':naviLinks}))        
         
     # Elektrotechnik, Informationstechnik, Physik
     def fakultaet4(self):
         html = urllib2.urlopen(FAKULTAET_4).read()
         soup = BeautifulSoup(html)
-        self.response.write(soup.prettify())
+        naviLinks = self.getNaviLinks(soup)
+        self.response.write(json.dumps({'navi':naviLinks}))
         
     # Geistes- und Erziehungswissenschaften
     def fakultaet5(self):
         html = urllib2.urlopen(FAKULTAET_5).read()
         soup = BeautifulSoup(html)
-        self.response.write(soup.prettify())
+        naviLinks = self.getNaviLinks(soup)
+        self.response.write(json.dumps({'navi':naviLinks}))
         
     def fakultaet6(self):
         html = urllib2.urlopen(FAKULTAET_6).read()
         soup = BeautifulSoup(html)
-        self.response.write(soup.prettify())
+        naviLinks = self.getNaviLinks(soup)
+        self.response.write(json.dumps({'navi':naviLinks}))
         
+    def getNaviLinks(self, soup):
+        naviLinks = []
+        for divs in soup.find_all("div", class_="navi2"):
+            naviLinks.append(divs.a.string.strip().encode("utf-8"))
+            
+        return naviLinks
+    
+    def getInhalt(self, soup):
+        contents = []
+        for content in soup.find("div", class_="inhalt").find_all('p'):
+            contents.append(content.string.strip().encode("utf-8"))
+        
+        return contents
+    
+    def getInhaltLinks(self, soup): 
+        contentLinks = []
+        for contentLink in soup.find('div', class_="inhalt").find_all('li'):
+            contentLinks.append(contentLink.a.string.strip().encode("utf-8"))
+            
+        return contentLinks
+        
+    
+    
 application = webapp2.WSGIApplication([
     ('/fakultaeten/.*', Fakultaeten),
 ], debug=True)
